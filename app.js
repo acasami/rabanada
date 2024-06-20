41517c91f1fd29d52813c2a91dbd3918fd64ac81
Marley <36645395+acasami@users.noreply.github.com>
2024-06-20 09:17:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 let listaDeNumerosSorteados = [];
-let numeroLimite = 60;
+let numeroLimite = 65;
 let numeroSecreto = gerarNumeroAleatorio();
 console.log(numeroSecreto);
 
@@ -13,7 +13,7 @@ function exibirTextoNaTela(tag, texto){
 }
 function MensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 60');
+    exibirTextoNaTela('p', `Escolha um número entre 1 e ${numeroLimite}`);
 }
 
 MensagemInicial();
